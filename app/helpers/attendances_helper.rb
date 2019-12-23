f50d66dd391014bc475e73c117b31bc1dfcbb3c2
EC2 Default User <ec2-user@ip-172-31-33-62.us-east-2.compute.internal>
2019-12-23 14:01:58 +0000

    追加機能10完了

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -14,4 +14,26 @@ module AttendancesHelper
   def working_times(start, finish)
     format("%.2f", (((finish - start) / 60) / 60.0))
   end
+  
+   # 1. 出勤時間と退勤時間どちらも空欄である場合、次の繰り返し処理が続行される。
+   # 2. （1の条件式 がfalseだった場合）出勤時間か、
+   # または退勤時間が空欄の場合attendances_invalid?メソッドは繰り返し処理を終了し、falseを返す。
+   # 3. （2の条件式がfalseだった場合）出勤時間が退勤時間より大きい場合（出勤時間が退社時間より後の時間）
+   # attendances_invalid?メソッドは繰り返し処理を終了し、falseを返す
+   
+  def attendances_invalid?
+    attendances = true
+    attendances_params.each do |id, item|
+      if item[:started_at].blank? && item[:finished_at].blank?
+        next
+      elsif item[:started_at].blank? || item[:finished_at].blank?
+        attendances = false
+        break
+      elsif item[:started_at] > item[:finished_at]
+        attendances = false
+        break
+      end
+    end
+    return attendances
+  end
 end
